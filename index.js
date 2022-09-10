974dda3e3aa74703f7973829303a7cb50ecacd93
subhankar-sharma <109143468+subhankar-sharma@users.noreply.github.com>
2022-09-10 08:26:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var randomNumber1 = Math.floor(Math.random()*6) + 1;
 // alert("hey");
-var randomDiceImage  = "dice" + randomNumber1 + ".png";
+// var randomDiceImage  = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "images/" + randomDiceImage;
+var randomImageSource = "images/dice" + randomNumber1 + ".png";
 
 var image1 = document.querySelectorAll("img")[0];
 
